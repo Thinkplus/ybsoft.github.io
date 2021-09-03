832b62154bd013a16e5373cf439743ab2754eb97
정영배 <youngbae.jeong@seavantage.com>
2021-09-03 14:36:34 +0900

    Nomad Challenge Initial
    - 초기 커밋입니다.

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector("#clock");
+
+function Clock()
+{
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth()).padStart(2,"0");
+    const day = String(now.getDate()).padStart(2,"0");;
+    const hour = String(now.getHours()).padStart(2,"0");;
+    const minute = String(now.getMinutes()).padStart(2,"0");;
+    const second = String(now.getSeconds()).padStart(2,"0");;
+
+    clock.innerText = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+
+}
+
+setInterval(Clock, 1000);
+Clock();
+
